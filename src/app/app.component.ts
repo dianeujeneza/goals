ddaafd8c49664e4e8b722ae0185d2d61fba1a4c2
Diane Ujeneza <dianesis17@gmail.com>
2019-08-23 08:29:57 +0200

    created angular application of goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Goals';
+export class AppComponent{
+  // title = 'Goals';
+  // goal = 'Watch Finding Nemo';
+  // goals:string[];
+
+  // goals: Goal[] = [
+  //   {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
+  //   {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
+  //   {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
+  //   {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
+  //   {id:5,name:'Solve math homework',description:'Damn Math'},
+  //   {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
+  // ];
+  constructor(){
+    // this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+
+  } 
 }
